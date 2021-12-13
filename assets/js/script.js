e1c7262300bd3a33f19f8956836369795dcef18c
Juan Chavez <juan@Juans-Air-12.home>
2021-12-12 22:05:58 -0800

    task now save to the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,24 +16,74 @@
 
 //function to load task form local storage
 // function to save task after editing it
-
-var currentDayEl = document.querySelector("#currentDay");
-var timeBlockContaier = document.querySelector(".container");
-var createTimeBlockEl = function(){
-    for (var i = 0; i<8; i++) {
-        var timeBlockEl = $("<div>").addClass("time-block");
-        var hourEl = $("<p>").addClass(".hour").text(x + "AM");
-        
-
-
-
-        
-    }
-}
 var setCurrentDay = function(){
     var day = moment().format("dddd, MMM Do YY");
     
-    currentDayEl.textContent = day;
+    //currentDayEl.textContent = day; //javascript 
+    $("#currentDay").text(day);
 }
 
+//initializing object for local storage of task
+var schedule = {
+    nine: "",
+    ten: "",
+    eleven: "",
+    twelve: "",
+    one: "",
+    two: "",
+    three: "",
+    four: "",
+    five: ""
+};
+
+//create the 8 time block elements
+var createTimeBlockEl = function(){
+    var x = 9;
+    if (x < 13){
+        
+        var blockHour = 9;
+        loadTask();
+        //var text = "example";
+        var timeBlockEl = $("<div>").addClass("time-block d-flex row");
+        var hourEl = $("<p>").addClass("hour col-1").text(x + "AM");
+        var textareaEl = $("<textarea>").addClass("textarea col-10").text(schedule.nine);
+        var saveBtnEl = $("<p>").addClass("saveBtn col-1").text("SAVE");
+        
+        timeBlockEl.append(hourEl);
+        timeBlockEl.append(textareaEl);
+        timeBlockEl.append(saveBtnEl);
+        $(".container").append(timeBlockEl);
+
+        x++;
+        if(x===12){
+            x=1;
+        }
+        createTimeBlockEl();
+    }
+    
+}
+
+//get local storage to page
+var loadTask = function(){
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+    console.log(schedule);
+}
+
+//save textarea content into local storage
+var saveTask = function() {
+    var textInBox = $("textarea").val();
+    
+    console.log(textInBox);
+    schedule.nine = textInBox;
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+
+}
+
+
 setCurrentDay();
+createTimeBlockEl();
+
+//saves tasks when SAVE Button is clicked
+$(".saveBtn").on("click", saveTask);
+
+
